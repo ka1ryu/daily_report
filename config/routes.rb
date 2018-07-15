ebc47d9f4e6aa2916f0579422dcb24cea4be5a3c
ka1ryu <ellegarden356@gmail.com>
2018-07-15 18:17:59 +0900

    ログインとその後の見た目作成＋完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/reports' => 'reports#all'
-  root 'users#index'
+  root 'sessions#new'
   get '/signin' => 'users#new'
   resources :users
-  post '/signin',  to: 'users#create'
-
+  get    '/login'   => 'sessions#new'
+  post   '/login'   => 'sessions#create'
+  delete '/logout'  => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
