0b4e61982b4aef3e4f4db0773210556d44175e49
ka1ryu <ellegarden356@gmail.com>
2018-07-16 14:11:40 +0900

    エラーメッセージの調整＋別ユーザーページのリダイレクト処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
+    if current_user == @user
+      #ユーザーが自分のページへのアクセスする場合
+      @user = User.find(params[:id])
+    elsif current_user.nil?
+      redirect_to('/login')
+    else
+      redirect_to current_user
+    end
   end
 
   def create
